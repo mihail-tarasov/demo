1e5cf272fbfa9616147ee6c3c606f78431e4bf91
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-05-21 20:46:14 +0300

    add headerHtml;footerHtml;

--- src/main/java/com/example/demo/controllers/homeController.java
+++ src/main/java/com/example/demo/controllers/homeController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class homeController {
     @GetMapping("/home")
     public String home(Model model){
-        model.addAttribute("title","Главная страничка.");
+        model.addAttribute("title","Домашняя страница.");
         return  "home";
     }
 }
