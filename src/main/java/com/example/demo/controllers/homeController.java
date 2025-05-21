fd65327d204c04e1264ed085aee78d7afe258acc
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-05-21 19:29:42 +0300

    add homeController;home.html;

--- src/main/java/com/example/demo/controllers/homeController.java
+++ src/main/java/com/example/demo/controllers/homeController.java
@@ -1,16 +1,12 @@
 package com.example.demo.controllers;
 
-
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class MainController {
-    @GetMapping("/")
-    public String first(Model model){
+public class homeController {
+    @GetMapping("/home")
+    public String home(Model model){
         model.addAttribute("title","Главная страничка.");
-        return  "first";
+        return  "home";
     }
-
 }
