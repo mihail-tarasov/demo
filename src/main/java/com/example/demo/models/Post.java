85b62743e80c701b1f5004cbf57a53ccdba83a68
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-05-22 22:21:32 +0300

    add repository;models

---
+++ src/main/java/com/example/demo/models/Post.java
@@ -0,0 +1,56 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title, anons, fullText;
+    private int views;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnons() {
+        return anons;
+    }
+
+    public void setAnons(String anons) {
+        this.anons = anons;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+}
