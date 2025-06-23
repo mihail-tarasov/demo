140c04721ab5f8a039b25ec528a681ca960f0fdc
mihail-tarasov <mihail.tarasov7@yandex.ru>
2025-06-23 20:41:36 +0300

    добавил форму обработки статей

--- src/main/java/com/example/demo/models/Post.java
+++ src/main/java/com/example/demo/models/Post.java
@@ -53,4 +53,13 @@ public class Post {
     public void setViews(int views) {
         this.views = views;
     }
+
+    public Post() {
+    }
+
+    public Post(String title, String anons, String fullText) {
+        this.title = title;
+        this.anons = anons;
+        this.fullText = fullText;
+    }
 }
